20f095cff67b53f03132ad777da0710ca40ce3c7
Nestorsito04 <nestorrafaelbot@gmail.com>
2025-06-19 02:21:21 -0400

    Update nestor.cpp

--- nestor.cpp
+++ nestor.cpp
@@ -38,3 +38,54 @@ void mostrarMenu() {
     cout << "10. Salir\n";
     cout << "\nSeleccione una opcion: ";
 }
+
+void agregarProducto() {
+    limpiarPantalla();
+    Producto nuevo;
+    cout << "\nNuevo Producto\n";
+    cout << "Codigo: ";
+    cin.ignore();
+    cin.getline(nuevo.codigo, 10);
+    cout << "Nombre: ";
+    cin.getline(nuevo.nombre, 30);
+    cout << "Precio: ";
+    cin >> nuevo.precio;
+    cout << "Stock: ";
+    cin >> nuevo.stock;
+    cout << "Categoria: ";
+    cin.ignore();
+    cin.getline(nuevo.categoria, 30);
+    nuevo.activo = true;
+
+    productos.push_back(nuevo);
+    cout << "Producto agregado con exito.\n";
+}
+
+void mostrarProductosActivos() {
+    limpiarPantalla();
+    if (productos.empty()) {
+        cout << "No hay productos almacenados.\n";
+        return;
+    }
+    cout << "\n Productos Activos \n";
+    for (size_t i = 0; i < productos.size(); i++) {
+        if (productos[i].activo) {
+            cout << i + 1 << ". Codigo: " << productos[i].codigo << ", Nombre: " << productos[i].nombre
+                 << ", Precio: " << productos[i].precio << ", Stock: " << productos[i].stock
+                 << ", Categoria: " << productos[i].categoria << endl;
+        }
+    }
+}
+
+void mostrarProductosPorCategoria() {
+    limpiarPantalla();
+    if (productos.empty()) {
+        cout << "No hay productos almacenados.\n";
+        return;
+    }
+    char categoria[30];
+    cout << "\nIngrese la categoria: ";
+    cin.ignore();
+    cin.getline(categoria, 30);
+
+    cout << "\n Productos en la categoria " << categoria << " \n";
